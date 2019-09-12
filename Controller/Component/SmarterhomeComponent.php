c276a7375fab1c68ec2c9b7fb9e210df55089335
Ian van den Heuvel <blueangelcpt@gmail.com>
2019-09-12 10:03:16 +0200

    Added meter number validation

--- Controller/Component/SmarterhomeComponent.php
+++ Controller/Component/SmarterhomeComponent.php
@@ -15,15 +15,41 @@ class SmarterhomeComponent extends Component {
 	}
 
 	public function purchase($transactionId, $meterNumber, $amount) {
+		$request = array(
+			'header' => array(
+				'Content-Type' => 'application/x-www-form-urlencoded',
+				'Connection' => 'keep-alive',
+				'Cache-Control' => 'no-cache'
+			)
+		);
 		$payload = array(
 			'token' => $this->settings['token'],
 			'meterNo' => $meterNumber,
 			'invoiceNo' => $transactionId,
 			'amount' => $amount / 100
 		);
-		$this->log('Smarterhomes API request: ' . json_encode($payload), $this->tag);
-		$result = $this->socket->post($this->settings['url'] . '/api/v1/prepaid/credit', $payload);
-		$this->log('Smarterhomes API response (N$' . $amount . ' for ' . $meterNumber . '): ' . $result, $this->tag);
+		$result = $this->socket->post($this->settings['url'] . '/api/v1/prepaid/credit', $payload, $request);
+		$this->log('Smarterhomes API request: ' . $this->socket->request['raw'], $this->tag);
+		$this->log('Smarterhomes API response (N$' . ($amount / 100) . ' for ' . $meterNumber . '): ' . $result, $this->tag);
 		return json_decode($result->body, true);
+	}
+
+	public function validate($meterNumber) {
+		$request = array(
+			'header' => array(
+				'Content-Type' => 'application/x-www-form-urlencoded',
+				'Connection' => 'keep-alive',
+				'Cache-Control' => 'no-cache'
+			)
+		);
+		$payload = array(
+			'token' => $this->settings['token'],
+			'meterSerial' => $meterNumber
+		);
+		$result = $this->socket->post($this->settings['url'] . '/api/meter-validation', $payload, $request);
+		$this->log('Smarterhomes validation request: ' . $this->socket->request['raw'], $this->tag);
+		$this->log('Smarterhomes validation response (' . $meterNumber . '): ' . $result, $this->tag);
+		$response = json_decode($result->body, true);
+		return (!$response['error']);
 	}
 }
